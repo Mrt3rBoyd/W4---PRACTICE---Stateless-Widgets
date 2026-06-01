0fbaff064bda7d141b3d75c3b2ffed87a0bcbb00
Lor Sophy <lsophy065@gmail.com>
2026-06-01 10:46:36 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,97 @@
+/*
+//Ex1
+Ex1
+import 'package:flutter/material.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          child: Column(
+            crossAxisAlignment: ,
+            children: [
+              Container(
+                child: Center(
+                  Row(
+                    Padding(
+                      "Travelling",
+                      
+                    ),
+                  ),
+                ),
+              ),
+
+              Container(
+                child: Center(
+                  Row(
+                    Padding(
+                      "Skating",
+                      
+                    ),
+                  ),
+                ),
+              ),
+              
+            ],     
+          ),
+        ),
+      ),
+    ),
+  ),
+}
+*/
+
+
+
+
+
+/*
+//Ex2
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text("Submit", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text("Time", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text("Account", style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+*/
+
